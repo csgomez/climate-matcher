ef65a8e49039693313359aa6b3191c4ff0bd8eb3
Cesar <csgomez@users.noreply.github.com>
2023-09-01 00:53:56 -0500

    Settings modal can be closed by clicking outside of it.

--- src/components/SettingsModal.jsx
+++ src/components/SettingsModal.jsx
@@ -1,13 +1,18 @@
 import Modal from 'react-modal';
 import Settings from './Settings';
-import { useSelector } from 'react-redux';
-import { selectIsModalOpen } from '../reducers/modalSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { hideModal, selectIsModalOpen } from '../reducers/modalSlice';
 
 Modal.setAppElement('#root');
 
 const SettingsModal = () => {
-  // const [showModal, setShowModal] = useState(false);
+  const dispatch = useDispatch();
+
   const isModalOpen = useSelector(selectIsModalOpen);
+
+  const handleCloseModal = () => {
+    dispatch(hideModal());
+  };
 
   return (
     <Modal
@@ -21,6 +26,8 @@ const SettingsModal = () => {
           paddin: 0,
           display: 'flex',
           backgroundColor: 'rgb(31 31 31)',
+          border: '4px solid #ffffff33',
+          borderRadius: '3px',
         },
         overlay: {
           backgroundColor: 'rgba(0, 0, 0, 0.808)',
@@ -29,6 +36,8 @@ const SettingsModal = () => {
           alignItems: 'center',
         },
       }}
+      shouldCloseOnOverlayClick={true}
+      onRequestClose={handleCloseModal}
     >
       <Settings />
     </Modal>
